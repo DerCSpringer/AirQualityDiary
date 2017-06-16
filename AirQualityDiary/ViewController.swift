217ee1129911a80f557281988709c4b97f01ad42
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-06-16 11:32:58 -0700

    Initial Commit

---
+++ AirQualityDiary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AirQualityDiary
+//
+//  Created by Daniel Springer on 6/16/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
