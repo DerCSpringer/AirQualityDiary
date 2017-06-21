6943608e4d0a6f7aeeab7974e6213cbfb7d79fa0
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-06-21 15:33:50 -0700

    added DiaryEntryTableViewCell

---
+++ Cells/DiaryEntryTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DiaryEntryTableViewCell.swift
+//  AirQualityDiary
+//
+//  Created by Daniel Springer on 6/21/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import UIKit
+import Action
+import RxSwift
+
+class DiaryEntryTableViewCell: UITableViewCell {
+    
+    
+}
