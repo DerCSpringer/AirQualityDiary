216580ef5fa7f6e2b5e8c9d61b0cb5cb9fc16c04
Daniel Springer <dcs@Daniels-MacBook-Pro.local>
2017-06-21 15:06:22 -0700

    added ViewControllers

---
+++ Controllers/AddDiaryEntryViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddDiaryEntryViewController.swift
+//  AirQualityDiary
+//
+//  Created by Daniel Springer on 6/21/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import Foundation
+
+class AddDiaryEntryViewController: UIViewController, BindableType {
+    
+}
---
+++ Controllers/DiaryEntriesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DiaryEntriesViewController.swift
+//  AirQualityDiary
+//
+//  Created by Daniel Springer on 6/21/17.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import Foundation
+
+class DiaryEntriesViewController: UIViewController, BindableType {
+
+}
